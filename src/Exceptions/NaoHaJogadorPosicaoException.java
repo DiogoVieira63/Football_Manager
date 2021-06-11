4b3068f35dd21d49b52d86e605a528925bbb78fc
diogovieira63 <a93251@alunos.uminho.pt>
2021-06-11 22:46:31 +0100

    add naoHaJogador Exception

---
+++ src/Exceptions/NaoHaJogadorPosicaoException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class NaoHaJogadorPosicaoException extends Exception{
+    public NaoHaJogadorPosicaoException(){
+        super();
+    }
+
+    public NaoHaJogadorPosicaoException(String s){
+        super(s);
+    }
+}
